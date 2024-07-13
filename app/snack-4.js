d5ae7f906015b003e84f07e0e6d9221adf05f9e3
gabrielgrillo <gabrielgrillo_4@hotmail.com>
2024-07-13 18:05:56 +0200

    snack 4

--- app/snack-4.js
+++ app/snack-4.js
@@ -1,1 +1,25 @@
 console.log("snack4")
+
+// In un array sono contenuti i nomi degli invitati alla festa del grande Gatsby,
+//chiedi all’utente il suo nome e comunicagli se può partecipare o no alla festa.
+
+const listaInvitati = ['Gabriel', 'Ginevra', 'Mario', 'Giovanna', 'Giuseppe']
+console.log(listaInvitati)
+const invitoElement = document.getElementById("invito")
+
+const nomeInvitato = prompt("Inserisci il tuo nome")
+// let nomeFormat = nomeInvitato.toLowerCase()
+// console.log(nomeFormat)
+
+const trovato = listaInvitati.includes(nomeInvitato)
+console.log(trovato)
+
+if (trovato === true) {
+    console.log("sei stato invitato alla festa")
+    invitoElement.innerHTML = "Sei stato invitato alla festa"
+}
+else {
+    console.log("non sei stato invitato alla festa")
+    invitoElement.innerHTML = "Non sei stato invitato alla festa"
+
+}
